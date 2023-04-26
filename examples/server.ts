f9f47eea3f1068290be1e09301449e54964b1330
Alisson <thelinuxlich@gmail.com>
2023-04-26 18:54:52 -0300

    formatting with 2 spaces

--- examples/server.ts
+++ examples/server.ts
@@ -5,224 +5,226 @@ import { z } from "zod";
 import { bearerAuthScheme, openApiBuilder } from "../src";
 
 const userSchema = z.object({
-	id: z.string(),
-	name: z.string(),
-	email: z.string().email(),
+  id: z.string(),
+  name: z.string(),
+  email: z.string().email(),
 });
 
 const commentSchema = z.object({
-	id: z.string(),
-	content: z.string(),
-	createdAt: z.string(),
-	modifiedAt: z.string(),
+  id: z.string(),
+  content: z.string(),
+  createdAt: z.string(),
+  modifiedAt: z.string(),
 });
 
 type User = z.infer<typeof userSchema>;
 
 const errors = makeErrors([
-	{
-		status: 404,
-		description: "No users found",
-		schema: z.object({
-			message: z.enum(["No users found", "User not found"]),
-		}),
-		example: {
-			message: "No users found",
-		},
-	},
-	{
-		status: "default",
-		description: "Default error",
-		schema: z.object({
-			message: z.string(),
-		}),
-	},
+  {
+    status: 404,
+    description: "No users found",
+    schema: z.object({
+      message: z.enum(["No users found", "User not found"]),
+    }),
+    example: {
+      message: "No users found",
+    },
+  },
+  {
+    status: "default",
+    description: "Default error",
+    schema: z.object({
+      message: z.string(),
+    }),
+  },
 ]);
 
 const api = makeApi([
-	{
-		method: "get",
-		path: "/users",
-		alias: "getUsers",
-		description: "Get all users",
-		parameters: [
-			{
-				name: "limit",
-				type: "Query",
-				description: "Limit the number of users",
-				schema: z.number().positive().default(10),
-				example: 10,
-			},
-			{
-				name: "offset",
-				type: "Query",
-				description: "Offset the number of users",
-				schema: z.number().positive().optional(),
-				example: 1,
-			},
-			{
-				name: "filter",
-				type: "Query",
-				description: "Filter users by name",
-				schema: z.array(z.string()).refine((array) => array.length === new Set(array).size, {
-					message: "Duplicate values are not allowed",
-				}),
-			},
-		],
-		response: z.array(userSchema),
-		errors,
-	},
-	{
-		method: "get",
-		path: "/users/:id",
-		alias: "getUser",
-		description: "Get a user by id",
-		response: userSchema,
-		examples: {
-			Dexter: {
-				value: {
-					id: "1",
-					name: "Dexter",
-					email: "dexter@bluesky.com",
-				},
-			},
-			Rita: {
-				value: {
-					id: "2",
-					name: "Rita",
-					email: "rita@bluesky.com",
-				},
-			},
-		},
-		errors,
-	},
-	{
-		method: "get",
-		path: "/users/:id/comments",
-		alias: "getComments",
-		description: "Get all user comments",
-		response: z.array(commentSchema),
-		errors,
-	},
-	{
-		method: "get",
-		path: "/users/:id/comments/:commentId",
-		alias: "getComment",
-		description: "Get a user comment by id",
-		response: commentSchema,
-		errors,
-	},
+  {
+    method: "get",
+    path: "/users",
+    alias: "getUsers",
+    description: "Get all users",
+    parameters: [
+      {
+        name: "limit",
+        type: "Query",
+        description: "Limit the number of users",
+        schema: z.number().positive().default(10),
+        example: 10,
+      },
+      {
+        name: "offset",
+        type: "Query",
+        description: "Offset the number of users",
+        schema: z.number().positive().optional(),
+        example: 1,
+      },
+      {
+        name: "filter",
+        type: "Query",
+        description: "Filter users by name",
+        schema: z
+          .array(z.string())
+          .refine((array) => array.length === new Set(array).size, {
+            message: "Duplicate values are not allowed",
+          }),
+      },
+    ],
+    response: z.array(userSchema),
+    errors,
+  },
+  {
+    method: "get",
+    path: "/users/:id",
+    alias: "getUser",
+    description: "Get a user by id",
+    response: userSchema,
+    examples: {
+      Dexter: {
+        value: {
+          id: "1",
+          name: "Dexter",
+          email: "dexter@bluesky.com",
+        },
+      },
+      Rita: {
+        value: {
+          id: "2",
+          name: "Rita",
+          email: "rita@bluesky.com",
+        },
+      },
+    },
+    errors,
+  },
+  {
+    method: "get",
+    path: "/users/:id/comments",
+    alias: "getComments",
+    description: "Get all user comments",
+    response: z.array(commentSchema),
+    errors,
+  },
+  {
+    method: "get",
+    path: "/users/:id/comments/:commentId",
+    alias: "getComment",
+    description: "Get a user comment by id",
+    response: commentSchema,
+    errors,
+  },
 ]);
 
 const adminApi = makeApi([
-	{
-		method: "post",
-		path: "/users",
-		alias: "createUser",
-		description: "Create a user",
-		parameters: [
-			{
-				name: "user",
-				type: "Body",
-				description: "The user to create",
-				schema: userSchema.omit({ id: true }),
-			},
-		],
-		response: userSchema,
-		errors,
-	},
-	{
-		method: "put",
-		path: "/users/:id",
-		alias: "updateUser",
-		description: "Update a user",
-		parameters: [
-			{
-				name: "user",
-				type: "Body",
-				description: "The user to update",
-				schema: userSchema,
-			},
-		],
-		response: userSchema,
-		errors,
-	},
-	{
-		method: "delete",
-		path: "/users/:id",
-		alias: "deleteUser",
-		description: "Delete a user",
-		response: z.void(),
-		status: 204,
-		errors,
-	},
+  {
+    method: "post",
+    path: "/users",
+    alias: "createUser",
+    description: "Create a user",
+    parameters: [
+      {
+        name: "user",
+        type: "Body",
+        description: "The user to create",
+        schema: userSchema.omit({ id: true }),
+      },
+    ],
+    response: userSchema,
+    errors,
+  },
+  {
+    method: "put",
+    path: "/users/:id",
+    alias: "updateUser",
+    description: "Update a user",
+    parameters: [
+      {
+        name: "user",
+        type: "Body",
+        description: "The user to update",
+        schema: userSchema,
+      },
+    ],
+    response: userSchema,
+    errors,
+  },
+  {
+    method: "delete",
+    path: "/users/:id",
+    alias: "deleteUser",
+    description: "Delete a user",
+    response: z.void(),
+    status: 204,
+    errors,
+  },
 ]);
 
 const app = zodiosApp();
 const userRouter = zodiosRouter([...api, ...adminApi]);
 
 const users: User[] = [
-	{
-		id: "1",
-		name: "John Doe",
-		email: "john.doe@test.com",
-	},
-	{
-		id: "2",
-		name: "John Doe",
-		email: "john.doe@test.com",
-	},
+  {
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@test.com",
+  },
+  {
+    id: "2",
+    name: "John Doe",
+    email: "john.doe@test.com",
+  },
 ];
 
 userRouter.get("/users", (req, res) => {
-	res.json(users);
+  res.json(users);
 });
 
 userRouter.get("/users/:id", (req, res) => {
-	const user = users.find((user) => user.id === req.params.id);
-	if (!user) {
-		res.status(404).json({ message: "User not found" });
-	}
-	return res.json(user);
+  const user = users.find((user) => user.id === req.params.id);
+  if (!user) {
+    res.status(404).json({ message: "User not found" });
+  }
+  return res.json(user);
 });
 
 userRouter.post("/users", (req, res) => {
-	const user = { ...req.body, id: String(users.length + 1) };
-	users.push(user);
-	return res.json(user);
+  const user = { ...req.body, id: String(users.length + 1) };
+  users.push(user);
+  return res.json(user);
 });
 
 userRouter.put("/users/:id", (req, res) => {
-	const userIdx = users.findIndex((user) => user.id === req.params.id);
-	if (userIdx < 0) {
-		res.status(404).json({ message: "User not found" });
-	}
-	const updatedUser = { ...users[userIdx], ...req.body };
-	users[userIdx] = updatedUser;
-	return res.json(updatedUser);
+  const userIdx = users.findIndex((user) => user.id === req.params.id);
+  if (userIdx < 0) {
+    res.status(404).json({ message: "User not found" });
+  }
+  const updatedUser = { ...users[userIdx], ...req.body };
+  users[userIdx] = updatedUser;
+  return res.json(updatedUser);
 });
 
 userRouter.delete("/users/:id", (req, res) => {
-	const userIdx = users.findIndex((user) => user.id === req.params.id);
-	if (userIdx < 0) {
-		res.status(404).json({ message: "User not found" });
-	}
-	users.splice(userIdx, 1);
-	return res.status(204).send();
+  const userIdx = users.findIndex((user) => user.id === req.params.id);
+  if (userIdx < 0) {
+    res.status(404).json({ message: "User not found" });
+  }
+  users.splice(userIdx, 1);
+  return res.status(204).send();
 });
 
 app.use("/api/v1", userRouter);
 
 const document = openApiBuilder({
-	title: "User API",
-	version: "1.0.0",
-	description: "A simple user API",
+  title: "User API",
+  version: "1.0.0",
+  description: "A simple user API",
 })
-	.addServer({ url: "/api/v1" })
-	.addSecurityScheme("admin", bearerAuthScheme())
-	.addPublicApi(api)
-	.addProtectedApi("admin", adminApi)
-	.build();
+  .addServer({ url: "/api/v1" })
+  .addSecurityScheme("admin", bearerAuthScheme())
+  .addPublicApi(api)
+  .addProtectedApi("admin", adminApi)
+  .build();
 
 app.use(`/docs/swagger.json`, (_, res) => res.json(document));
 app.use("/docs", serve);
--- src/openapi.test.ts
+++ src/openapi.test.ts
@@ -1,249 +1,257 @@
 import { makeApi } from "@zodios/core";
 import { z } from "zod";
-import { toOpenApi, basicAuthScheme, bearerAuthScheme, oauth2Scheme, openApiBuilder } from "./openapi";
+import {
+  toOpenApi,
+  basicAuthScheme,
+  bearerAuthScheme,
+  oauth2Scheme,
+  openApiBuilder,
+} from "./openapi";
 
 const user = z.object({
-	id: z.string(),
-	name: z.string(),
-	email: z.string().email(),
+  id: z.string(),
+  name: z.string(),
+  email: z.string().email(),
 });
 
 const api = makeApi([
-	{
-		method: "get",
-		path: "/users?filter=:filter#fragment",
-		alias: "getUsers",
-		description: "Get all users",
-		parameters: [
-			{
-				name: "limit",
-				type: "Query",
-				description: "Limit the number of users",
-				schema: z.number().positive().default(10),
-				example: 10,
-			},
-			{
-				name: "offset",
-				type: "Query",
-				description: "Offset the number of users",
-				schema: z.number().positive().optional(),
-				example: 1,
-			},
-			{
-				name: "filter",
-				type: "Query",
-				description: "Filter users by name",
-				schema: z.array(z.string()).refine((a) => new Set(a).size === a.length, "No duplicates allowed"),
-			},
-		],
-		response: z.array(user),
-		examples: {
-			Dexter: {
-				value: {
-					id: "1",
-					name: "Dexter",
-					email: "dexter@bluesky.com",
-				},
-			},
-		},
-		errors: [
-			{
-				status: 404,
-				description: "No users found",
-				schema: z.object({
-					message: z.literal("No users found"),
-				}),
-				example: {
-					message: "No users found",
-				},
-			},
-			{
-				status: "default",
-				description: "Default error",
-				schema: z.object({
-					message: z.string(),
-				}),
-			},
-		],
-	},
-	{
-		method: "get",
-		path: "/users/:id",
-		alias: "getUser",
-		description: "Get a user by id",
-		response: user,
-	},
-	{
-		method: "post",
-		path: "/users",
-		alias: "createUser",
-		description: "Create a user",
-		parameters: [
-			{
-				name: "user",
-				type: "Body",
-				description: "The user to create",
-				schema: user.omit({ id: true }),
-				example: {
-					name: "Rita",
-					email: "rita@bluesky.com",
-				},
-			},
-		],
-		response: user,
-	},
-	{
-		method: "put",
-		path: "/users/:id",
-		alias: "updateUser",
-		description: "Update a user",
-		parameters: [
-			{
-				name: "user",
-				type: "Body",
-				description: "The user to update",
-				schema: user,
-			},
-		],
-		response: user,
-	},
-	{
-		method: "delete",
-		path: "/users/:id",
-		alias: "deleteUser",
-		description: "Delete a user",
-		response: z.void(),
-		status: 204,
-	},
+  {
+    method: "get",
+    path: "/users?filter=:filter#fragment",
+    alias: "getUsers",
+    description: "Get all users",
+    parameters: [
+      {
+        name: "limit",
+        type: "Query",
+        description: "Limit the number of users",
+        schema: z.number().positive().default(10),
+        example: 10,
+      },
+      {
+        name: "offset",
+        type: "Query",
+        description: "Offset the number of users",
+        schema: z.number().positive().optional(),
+        example: 1,
+      },
+      {
+        name: "filter",
+        type: "Query",
+        description: "Filter users by name",
+        schema: z
+          .array(z.string())
+          .refine((a) => new Set(a).size === a.length, "No duplicates allowed"),
+      },
+    ],
+    response: z.array(user),
+    examples: {
+      Dexter: {
+        value: {
+          id: "1",
+          name: "Dexter",
+          email: "dexter@bluesky.com",
+        },
+      },
+    },
+    errors: [
+      {
+        status: 404,
+        description: "No users found",
+        schema: z.object({
+          message: z.literal("No users found"),
+        }),
+        example: {
+          message: "No users found",
+        },
+      },
+      {
+        status: "default",
+        description: "Default error",
+        schema: z.object({
+          message: z.string(),
+        }),
+      },
+    ],
+  },
+  {
+    method: "get",
+    path: "/users/:id",
+    alias: "getUser",
+    description: "Get a user by id",
+    response: user,
+  },
+  {
+    method: "post",
+    path: "/users",
+    alias: "createUser",
+    description: "Create a user",
+    parameters: [
+      {
+        name: "user",
+        type: "Body",
+        description: "The user to create",
+        schema: user.omit({ id: true }),
+        example: {
+          name: "Rita",
+          email: "rita@bluesky.com",
+        },
+      },
+    ],
+    response: user,
+  },
+  {
+    method: "put",
+    path: "/users/:id",
+    alias: "updateUser",
+    description: "Update a user",
+    parameters: [
+      {
+        name: "user",
+        type: "Body",
+        description: "The user to update",
+        schema: user,
+      },
+    ],
+    response: user,
+  },
+  {
+    method: "delete",
+    path: "/users/:id",
+    alias: "deleteUser",
+    description: "Delete a user",
+    response: z.void(),
+    status: 204,
+  },
 ]);
 
 describe("toOpenApi", () => {
-	it("should generate bearer scheme", () => {
-		const scheme = bearerAuthScheme();
-		expect(scheme).toEqual({
-			type: "http",
-			scheme: "bearer",
-			bearerFormat: "JWT",
-		});
-	});
+  it("should generate bearer scheme", () => {
+    const scheme = bearerAuthScheme();
+    expect(scheme).toEqual({
+      type: "http",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+    });
+  });
 
-	it("should generate basic scheme", () => {
-		const scheme = basicAuthScheme();
-		expect(scheme).toEqual({
-			type: "http",
-			scheme: "basic",
-		});
-	});
+  it("should generate basic scheme", () => {
+    const scheme = basicAuthScheme();
+    expect(scheme).toEqual({
+      type: "http",
+      scheme: "basic",
+    });
+  });
 
-	it("should generate oauth2 scheme", () => {
-		const scheme = oauth2Scheme({
-			implicit: {
-				authorizationUrl: "https://example.com/oauth2/authorize",
-				scopes: {
-					read: "Read access",
-					write: "Write access",
-				},
-			},
-		});
-		expect(scheme).toEqual({
-			type: "oauth2",
-			flows: {
-				implicit: {
-					authorizationUrl: "https://example.com/oauth2/authorize",
-					scopes: {
-						read: "Read access",
-						write: "Write access",
-					},
-				},
-			},
-		});
-	});
+  it("should generate oauth2 scheme", () => {
+    const scheme = oauth2Scheme({
+      implicit: {
+        authorizationUrl: "https://example.com/oauth2/authorize",
+        scopes: {
+          read: "Read access",
+          write: "Write access",
+        },
+      },
+    });
+    expect(scheme).toEqual({
+      type: "oauth2",
+      flows: {
+        implicit: {
+          authorizationUrl: "https://example.com/oauth2/authorize",
+          scopes: {
+            read: "Read access",
+            write: "Write access",
+          },
+        },
+      },
+    });
+  });
 
-	it("should convert to openapi", () => {
-		const openApi = toOpenApi(api, {
-			info: {
-				title: "My API",
-				version: "1.0.0",
-			},
-		});
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi", () => {
+    const openApi = toOpenApi(api, {
+      info: {
+        title: "My API",
+        version: "1.0.0",
+      },
+    });
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with prefix", () => {
-		const openApi = toOpenApi(api, {
-			info: {
-				title: "My Prefixed API",
-				version: "1.0.0",
-			},
-			servers: [
-				{
-					url: "/api/v1",
-				},
-			],
-		});
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with prefix", () => {
+    const openApi = toOpenApi(api, {
+      info: {
+        title: "My Prefixed API",
+        version: "1.0.0",
+      },
+      servers: [
+        {
+          url: "/api/v1",
+        },
+      ],
+    });
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with bearer auth", () => {
-		const openApi = toOpenApi(api, {
-			info: {
-				title: "My Bearer API",
-				version: "1.0.0",
-			},
-			securityScheme: bearerAuthScheme(),
-		});
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with bearer auth", () => {
+    const openApi = toOpenApi(api, {
+      info: {
+        title: "My Bearer API",
+        version: "1.0.0",
+      },
+      securityScheme: bearerAuthScheme(),
+    });
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with basic auth", () => {
-		const openApi = toOpenApi(api, {
-			info: {
-				title: "My Basic API",
-				version: "1.0.0",
-			},
-			securityScheme: basicAuthScheme(),
-		});
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with basic auth", () => {
+    const openApi = toOpenApi(api, {
+      info: {
+        title: "My Basic API",
+        version: "1.0.0",
+      },
+      securityScheme: basicAuthScheme(),
+    });
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with oauth2 auth", () => {
-		const openApi = toOpenApi(api, {
-			info: {
-				title: "My OAuth2 API",
-				version: "1.0.0",
-			},
-			securityScheme: oauth2Scheme({
-				implicit: {
-					authorizationUrl: "https://example.com/oauth2/authorize",
-					scopes: {
-						read: "Read access",
-						write: "Write access",
-					},
-				},
-			}),
-		});
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with oauth2 auth", () => {
+    const openApi = toOpenApi(api, {
+      info: {
+        title: "My OAuth2 API",
+        version: "1.0.0",
+      },
+      securityScheme: oauth2Scheme({
+        implicit: {
+          authorizationUrl: "https://example.com/oauth2/authorize",
+          scopes: {
+            read: "Read access",
+            write: "Write access",
+          },
+        },
+      }),
+    });
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with builder", () => {
-		const openApi = openApiBuilder({
-			title: "My API",
-			version: "1.0.0",
-		})
-			.addPublicApi(api)
-			.build();
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with builder", () => {
+    const openApi = openApiBuilder({
+      title: "My API",
+      version: "1.0.0",
+    })
+      .addPublicApi(api)
+      .build();
+    expect(openApi).toMatchSnapshot();
+  });
 
-	it("should convert to openapi with builder with security", () => {
-		const openApi = openApiBuilder({
-			title: "My API",
-			version: "1.0.0",
-		})
-			.addServer({ url: "/api/v1" })
-			.addSecurityScheme("auth", bearerAuthScheme())
-			.addProtectedApi("auth", api)
-			.build();
-		expect(openApi).toMatchSnapshot();
-	});
+  it("should convert to openapi with builder with security", () => {
+    const openApi = openApiBuilder({
+      title: "My API",
+      version: "1.0.0",
+    })
+      .addServer({ url: "/api/v1" })
+      .addSecurityScheme("auth", bearerAuthScheme())
+      .addProtectedApi("auth", api)
+      .build();
+    expect(openApi).toMatchSnapshot();
+  });
 });
